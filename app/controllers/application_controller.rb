2639d706bda180a5eaf2bae149880ba920338111
github username <github email address>
2020-04-02 02:14:17 -0700

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 
 require_relative '../../config/environment'
+require 'pry'
 
 class ApplicationController < Sinatra::Base
 
@@ -8,6 +9,57 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
+
+
   get '/' do
+    redirect to "/articles"
   end
+
+    #CREATE a new article
+
+  get '/articles/new' do
+    #@article = Article.new
+    erb :new
+  end
+  
+  post '/articles' do
+    @article = Article.create(params)
+    redirect to "/articles/#{@article.id}"
+  end
+
+  #READ (show a given article/ after creation /and index of all articles)
+
+  get '/articles/:id' do
+    @article = Article.find(params[:id])
+    erb :show
+  end
+  
+  get '/articles' do
+    @articles = Article.all 
+    erb :index
+  end
+
+  #EDIT an Article
+
+  get '/articles/:id/edit' do
+    @article = Article.find(params[:id])
+    erb :edit
+  end
+
+  patch '/articles/:id' do
+    @article = Article.find(params[:id])
+    @article.update(params[:article])
+    redirect to "/articles/#{@article.id}"
+    
+  end
+
+ #DELETE and article
+
+ delete '/articles/:id' do
+  @article = Article.find(params[:id])
+  @article.destroy
+  redirect to "/articles"
+ end
+ 
+
 end
